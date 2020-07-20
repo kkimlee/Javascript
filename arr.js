199b7efc97d9ee4809afe2f073bb86005a937d98
kkimlee <40078267+kkimlee@users.noreply.github.com>
2020-07-20 21:16:53 +0900

    Day 1

---
+++ arr.js
@@ -0,0 +1,9 @@
+var arr = [];
+console.log("arr = ", arr);
+
+var arr2 = [1, 2, 3, 4, 5];
+console.log("arr2 = ", arr2);
+
+var mixed_arr = [1, true, 3.14, "string", {name: "object"}, [1, 2, 3]];
+console.log("mixed_arr = ", mixed_arr);
+console.log("mixed_arr.length = ", mixed_arr.length);
---
+++ arr2.js
@@ -0,0 +1,21 @@
+var arr = [1, 2, 3, 4, 5];
+console.log("arr = ", arr);
+console.log("arr.pop = ", arr.pop);
+console.log("arr = ", arr);
+console.log("arr.shift() = ", arr.shift());
+console.log("arr = ", arr);
+console.log("arr.push(6) = ", arr.push(6));
+console.log("arr = ", arr);
+console.log("arr.unshift(0) = ", arr.unshift(0));
+console.log("arr = ", arr);
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+console.log("arr1 = ", arr1);
+console.log("arr2 = ", arr2);
+console.log("arr1.concat(arr2) = ", arr1.concat(arr2));
+console.log("arr1 = ", arr1);
+
+var str = "1, 2, 3, 4, 5";
+console.log("str = ", str);
+console.log("str.split(\",\"):", str.split(","));
